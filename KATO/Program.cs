4e3891af62a83c24f857c136675eea12eb02aa54
智洋 陶山 <t_suyama@neolis.co.jp>
2017-05-08 11:51:39 +0900

    20170508 陶山

--- KATO/Program.cs
+++ KATO/Program.cs
@@ -18,7 +18,7 @@ namespace KATO
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new KATO.Form.M_Daibunrui.M_Daibunrui());
 
-            //2017.04.21 kawab
+            //2017.04.21 suyama
 
         }
     }
