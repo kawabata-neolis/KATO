19f620033add82676abc88864765b23518aa8fd3
大河内　政宏 <m_ookochi@neolis.co.jp>
2017-07-06 18:01:33 +0900

    画面サイズを一部修正

--- KATO/Form/C0490_UriageSuiiHyo/C0490_UriageSuiiHyo.Designer.cs
+++ KATO/Form/C0490_UriageSuiiHyo/C0490_UriageSuiiHyo.Designer.cs
@@ -397,7 +397,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.ClientSize = new System.Drawing.Size(1424, 826);
             this.Controls.Add(this.labelSet_TokuisakiEnd);
             this.Controls.Add(this.labelSet_TokuisakiStart);
             this.Controls.Add(this.txtCalendarYMclose);
