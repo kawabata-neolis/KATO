8698c62535f60a3e73577419bea4936eae5ab6f4
智洋 陶山 <t_suyama@neolis.co.jp>
2017-07-28 17:17:32 +0900

    外注様作成分 追加（7/28）

---
+++ KATO/Common/Util/CreatePdf.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Data;
+using System.ComponentModel;
+
+using Spire.Xls;
+
+//iTextSharp関連の名前空間
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using ClosedXML.Excel;
+
+namespace KATO.Common.Util
+{
+    class CreatePdf
+    {
+        /// -----------------------------------------------------------------------------
+        /// <summary>
+        /// PDF化(Spire.xls)の処理
+        /// <param name="strInXlsFile">エクセルファイル</param>
+        /// <param name="strDateTime">日時</param>
+        /// </summary>
+        /// -----------------------------------------------------------------------------
+        public string createPdf(string strInXlsFile, string strDateTime)
+        {
+            string strWorkPath = System.Configuration.ConfigurationManager.AppSettings["workpath"];
+            string strPdfPath = System.Configuration.ConfigurationManager.AppSettings["pdfpath"];
+            string strJoinPdfFile;
+
+            try
+            {
+                Workbook printbook = new Workbook();
+                printbook.LoadFromFile(strInXlsFile, ExcelVersion.Version2010);
+                int sheetMax = printbook.Worksheets.Count;
+
+                // Excelシートの枚数分PDF化
+                for (int sheetCnt = 0; sheetCnt < sheetMax; sheetCnt++)
+                {
+                    // pdf化するシートを取得
+                    Worksheet printsheet = printbook.Worksheets[sheetCnt];
+
+                    string no = no = (sheetCnt + 1).ToString();
+                    if (no.Length == 1)
+                    {
+                        no = "0" + no;
+                    }
+
+                    string strPdfFile = strWorkPath + strDateTime + "_" + no + ".pdf";
+
+                    // 出力したいシートをPDFで保存
+                    printsheet.SaveToPdf(strPdfFile);
+
+                    // シートカウントが0の場合結合用のPDFを保存
+                    if (sheetCnt == 0)
+                    {
+                        string strJoinyouPdfFile = strPdfPath + strDateTime + ".pdf";
+
+                        // 出力したいシートをPDFで保存
+                        printsheet.SaveToPdf(strJoinyouPdfFile);
+                    }
+                }
+                // printbookを解放
+                printbook.Dispose();
+
+                // フォルダ下の作成日時".pdf"ファイルをすべて取得する
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(strWorkPath);
+                System.IO.FileInfo[] fiFiles = di.GetFiles(strDateTime + "*.pdf", System.IO.SearchOption.AllDirectories);
+                Array.Sort<FileInfo>(fiFiles, delegate (FileInfo f1, FileInfo f2)
+                {
+                    // ファイル名でソート
+                    return f1.Name.CompareTo(f2.Name);
+                });
+                int filesMax = fiFiles.Count();
+                string[] strFiles = new string[filesMax];
+
+                // FileInfo配列をstring配列に
+                for (int fileCnt = 0; fileCnt < filesMax; fileCnt++)
+                {
+                    strFiles[fileCnt] = strWorkPath + fiFiles[fileCnt].Name;
+                }
+
+                // 結合PDFオブジェクト
+                strJoinPdfFile = strPdfPath + strDateTime + ".pdf";
+
+                // PDFファイル数が0でなければ結合
+                if (filesMax != 0)
+                {
+                    fnJoinPdf(strFiles, strJoinPdfFile, 1);
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
+            return strJoinPdfFile;
+        }
+
+        /// -----------------------------------------------------------------------------
+        /// <summary>
+        /// PDFファイルの結合
+        /// WritePage = 0：全ページ、WritePage = 1：全ファイルの1ページのみ
+        /// WritePage = 2(3...)：全ファイルの1～2(1～3)ページ
+        /// </summary>
+        /// <param name="arySrcFilePath">入力ファイルパス</param>
+        /// <param name="sJoinFilePath">結合ファイルパス</param>
+        /// <param name="WritePage">結合ページ数</param>
+        /// -----------------------------------------------------------------------------
+        private void fnJoinPdf(string[] arySrcFilePath, string sJoinFilePath, int WritePage)
+        {
+            Document doc = null;    // 出力ファイルDocument
+            PdfCopy copy = null;    // 出力ファイルPdfCopy
+
+            try
+            {
+                //-------------------------------------------------------------------------------------
+                // ファイル件数分、ファイル結合
+                //-------------------------------------------------------------------------------------
+                for (int i = 0; i < arySrcFilePath.Length; i++)
+                {
+                    // リーダー取得
+                    PdfReader reader = new PdfReader(arySrcFilePath[i]);
+                    // 入力ファイル1を出力ファイルの雛形にする
+                    if (i == 0)
+                    {
+                        // Document作成
+                        doc = new Document(reader.GetPageSizeWithRotation(1));
+                        // 出力ファイルPdfCopy作成
+                        copy = new PdfCopy(doc, new FileStream(sJoinFilePath, FileMode.Create));
+                        // 出力ファイルDocumentを開く
+                        doc.Open();
+                        // 文章プロパティ設定
+                        //doc.AddKeywords((string)reader.Info["Keywords"]);
+                        //doc.AddAuthor((string)reader.Info["Author"]);
+                        //doc.AddTitle((string)reader.Info["Title"]);
+                        //doc.AddCreator((string)reader.Info["Creator"]);
+                        //doc.AddSubject((string)reader.Info["Subject"]);
+                    }
+                    // 結合するPDFのページ数
+                    if (WritePage == 0) WritePage = reader.NumberOfPages;
+                    if (WritePage > reader.NumberOfPages) WritePage = reader.NumberOfPages;
+
+                    // PDFコンテンツを取得、copyオブジェクトに追加
+                    for (int iPageCnt = 1; iPageCnt <= WritePage; iPageCnt++)
+                    {
+                        PdfImportedPage page = copy.GetImportedPage(reader, iPageCnt);
+                        copy.AddPage(page);
+                    }
+                    // フォーム入力を結合
+                    PRAcroForm form = reader.AcroForm;
+                    if (form != null)
+                        copy.AddDocument(reader);
+                    // リーダーを閉じる
+                    reader.Close();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (copy != null)
+                    copy.Close();
+                if (doc != null)
+                    doc.Close();
+            }
+        }
+
+        /// -----------------------------------------------------------------------------------------
+        /// <summary>
+        /// ListをDataTableへ変換
+        /// </summary>
+        /// -----------------------------------------------------------------------------------------
+        public DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection properties =
+                TypeDescriptor.GetProperties(typeof(T));
+
+            DataTable table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        /// -----------------------------------------------------------------------------
+        /// <summary>
+        /// ロゴ（KATO_LOGO）の貼り付け処理
+        /// </summary>
+        /// <param name="strInXlsFile">エクセルファイル</param>
+        /// <param name="topRow">貼り付け位置（行）</param>
+        /// <param name="leftColumn">貼り付け位置（列）</param>
+        /// <param name="topRowOffset">貼り付け位置（行：オフセット）</param>
+        /// <param name="LeftColumnOffset">貼り付け位置（列：オフセット）</param>
+        /// <param name="percent">縮小サイズ</param>
+        /// -----------------------------------------------------------------------------
+        public void logoPaste(string strInXlsFile, int[] topRow, int[] leftColumn, int topRowOffset, int leftColumnOffset, int percent)
+        {
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(strInXlsFile, ExcelVersion.Version2010);
+
+            for (int sheetCnt = 0; sheetCnt < workbook.Worksheets.Count; sheetCnt++)
+            {
+                Worksheet worksheet = workbook.Worksheets[sheetCnt];
+
+                for (int cnt = 0; cnt < topRow.Count(); cnt++)
+                {
+                    // 画像の貼り付け
+                    ExcelPicture pic = worksheet.Pictures.Add(topRow[cnt], leftColumn[cnt], @"./Common/KATO_LOGO.jpg");
+                    pic.TopRowOffset = topRowOffset;
+                    pic.LeftColumnOffset = leftColumnOffset;
+                    pic.Width = (int)(pic.Width * (float)percent / 100);
+                    pic.Height = (int)(pic.Height * (float)percent / 100);
+                }
+            }
+
+            // 保存
+            workbook.Save();
+
+            // workbookを解放
+            workbook.Dispose();
+        }
+
+        /// <summary>
+        /// ヘッダーシートをコピー
+        /// <param name="workbook">参照型 ワークブック</param>
+        /// <param name="headersheet">参照型 ヘッダーシート</param>
+        /// <param name="currentsheet">参照型 カレントシート</param>
+        /// <param name="pageCnt">ページ数</param>
+        /// </summary>
+        public void sheetCopy(ref XLWorkbook workbook, ref IXLWorksheet headersheet, ref IXLWorksheet currentsheet, int pageCnt)
+        {
+            // ヘッダーシートからコピー
+            headersheet.CopyTo("Page" + pageCnt.ToString());
+            currentsheet = workbook.Worksheet(pageCnt + 1);
+        }
+
+        /// -----------------------------------------------------------------------------
+        /// <summary>
+        /// ヘッダーシートをコピーし、ヘッダー部を指定
+        /// <param name="workbook">参照型 ワークブック</param>
+        /// <param name="headersheet">参照型 ヘッダーシート</param>
+        /// <param name="currentsheet">参照型 カレントシート</param>
+        /// <param name="page">ページ数</param>
+        /// <param name="page">最大ページ数</param>
+        /// <param name="strNow">現在の日付</param>
+        /// </summary>
+        /// -----------------------------------------------------------------------------
+        public void sheetCopy(ref XLWorkbook workbook, ref IXLWorksheet headersheet, ref IXLWorksheet currentsheet, int page, int maxPage, string strNow)
+        {
+            // ヘッダー部に指定する情報を取得
+            string strHeader = this.getHeader(page, maxPage, strNow);
+
+            // ヘッダーシートからコピー
+            headersheet.CopyTo("Page" + page.ToString());
+            currentsheet = workbook.Worksheet(page + 1);
+
+            // ヘッダー部の指定（コンピュータ名、日付、ページ数を出力）
+            currentsheet.PageSetup.Header.Right.AddText(strHeader);
+        }
+
+        /// -----------------------------------------------------------------------------
+        /// <summary>
+        /// ヘッダー部に指定する情報を取得
+        /// <param name="page">ページ数</param>
+        /// <param name="page">最大ページ数</param>
+        /// <param name="strNow">現在の日付</param>
+        /// </summary>
+        /// -----------------------------------------------------------------------------
+        public string getHeader(int page, int maxPage, string strNow)
+        {
+            string strSpace = "       ";
+
+            // コンピュータ名、日付、ページ数
+            string strHeader = "（ " + System.Windows.Forms.SystemInformation.ComputerName + " ）" +
+                strSpace + strNow + strSpace + page.ToString() + " / " + maxPage.ToString();
+
+            return strHeader;
+        }
+
+    }
+}
---
+++ KATO/Form/B0420_SeikyuMeisaishoPrint/B0420_SeikyuMeisaishoPrint.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KATO.Common.Ctl;
+using KATO.Common.Util;
+using static KATO.Common.Util.CommonTeisu;
+using KATO.Business.B0420_SeikyuMeisaishoPrint;
+
+namespace KATO.Form.B0420_SeikyuMeisaishoPrint
+{
+    /// <summary>
+    /// B0420_SeikyuMeisaishoPrint
+    /// 請求明細書フォーム
+    /// 作成者：多田
+    /// 作成日：2017/7/25
+    /// 更新者：多田
+    /// 更新日：2017/7/25
+    /// カラム論理名
+    /// </summary>
+    public partial class B0420_SeikyuMeisaishoPrint : BaseForm
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// B0420_SeikyuMeisaishoPrint
+        /// フォーム関係の設定
+        /// </summary>
+        public B0420_SeikyuMeisaishoPrint(Control c)
+        {
+            if (c == null)
+            {
+                return;
+            }
+
+            int intWindowWidth = c.Width;
+            int intWindowHeight = c.Height;
+
+            InitializeComponent();
+
+            // フォームが最大化されないようにする
+            this.MaximizeBox = false;
+            // フォームが最小化されないようにする
+            this.MinimizeBox = false;
+
+            // 最大サイズと最小サイズを現在のサイズに設定する
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+
+            // ウィンドウ位置をマニュアル
+            this.StartPosition = FormStartPosition.Manual;
+            // 親画面の中央を指定
+            this.Left = c.Left + (intWindowWidth - this.Width) / 2;
+            this.Top = c.Top + (intWindowHeight - this.Height) / 2;
+        }
+
+        /// <summary>
+        /// B0420_SeikyuMeisaishoPrint_Load
+        /// 読み込み時
+        /// </summary>
+        private void B0420_SeikyuMeisaishoPrint_Load(object sender, EventArgs e)
+        {
+            this.Show();
+            this._Title = "請求明細書";
+
+            // フォームでもキーイベントを受け取る
+            this.KeyPreview = true;
+
+            this.btnF04.Text = STR_FUNC_F4;
+            this.btnF11.Text = STR_FUNC_F11;
+            this.btnF12.Text = STR_FUNC_F12;
+
+            // 発行年月日の設定
+            txtHakkoYmd.setUp(0);
+        }
+
+        /// <summary>
+        /// B0420_SeikyuMeisaishoPrint_KeyDown
+        /// キー入力判定
+        /// </summary>
+        private void B0420_SeikyuMeisaishoPrint_KeyDown(object sender, KeyEventArgs e)
+        {
+            // キー入力情報によって動作を変える
+            switch (e.KeyCode)
+            {
+                case Keys.Tab:
+                    break;
+                case Keys.Left:
+                    break;
+                case Keys.Right:
+                    break;
+                case Keys.Up:
+                    break;
+                case Keys.Down:
+                    break;
+                case Keys.Delete:
+                    break;
+                case Keys.Back:
+                    break;
+                case Keys.Enter:
+                    break;
+                case Keys.F1:
+                    break;
+                case Keys.F2:
+                    break;
+                case Keys.F3:
+                    break;
+                case Keys.F4:
+                    logger.Info(LogUtil.getMessage(this._Title, "取消実行"));
+                    this.delText();
+                    break;
+                case Keys.F5:
+                    break;
+                case Keys.F6:
+                    break;
+                case Keys.F7:
+                    break;
+                case Keys.F8:
+                    break;
+                case Keys.F9:
+                    break;
+                case Keys.F10:
+                    break;
+                case Keys.F11:
+                    logger.Info(LogUtil.getMessage(this._Title, "印刷実行"));
+                    this.printReport();
+                    break;
+                case Keys.F12:
+                    logger.Info(LogUtil.getMessage(this._Title, "終了実行"));
+                    this.Close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// judBtnClick
+        /// ボタンの反応
+        /// </summary>
+        private void judBtnClick(object sender, EventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case STR_BTN_F04: // 取消
+                    logger.Info(LogUtil.getMessage(this._Title, "取消実行"));
+                    this.delText();
+                    break;
+                case STR_BTN_F11: // 印刷
+                    logger.Info(LogUtil.getMessage(this._Title, "印刷実行"));
+                    this.printReport();
+                    break;
+                case STR_BTN_F12: // 終了
+                    logger.Info(LogUtil.getMessage(this._Title, "終了実行"));
+                    this.Close();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// txtSimekiriYmd_Leave
+        /// 締切年月日の入力箇所からフォーカスが外れた時
+        /// </summary>
+        private void txtSimekiriYmd_Leave(object sender, EventArgs e)
+        {
+            // 締切年月日が空の場合
+            if (txtSimekiriYmd.blIsEmpty() == false)
+            {
+                return;
+            }
+
+            // ビジネス層のインスタンス生成
+            B0420_SeikyuMeisaishoPrint_B meisaiPrintB = new B0420_SeikyuMeisaishoPrint_B();
+            try
+            {
+                // 請求履歴を取得
+                DataTable dtGetsumatsu = meisaiPrintB.getGetsumatsu(txtSimekiriYmd.Text);
+
+                if (dtGetsumatsu != null && dtGetsumatsu.Rows.Count > 0)
+                {
+                    txtStartYmd.Text = dtGetsumatsu.Rows[0][0].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                // エラーロギング
+                new CommonException(ex);
+
+                // 例外発生メッセージ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_ERROR, CommonTeisu.LABEL_ERROR_MESSAGE, CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                return;
+            }
+        }
+
+        /// <summary>
+        /// delText
+        /// テキストボックス内の文字を削除
+        /// </summary>
+        private void delText()
+        {
+            txtSimekiriYmd.Text = "";
+            txtSimekiribi.Text = "";
+
+            txtSimekiriYmd.Focus();
+        }
+
+        /// <summary>
+        /// printReport
+        /// PDFを出力する
+        /// </summary>
+        private void printReport()
+        {
+            // データ検索用
+            List<string> lstSearchItem = new List<string>();
+
+            // データチェック
+            if (!blnDataCheck())
+            {
+                return;
+            }
+
+            // ビジネス層のインスタンス生成
+            B0420_SeikyuMeisaishoPrint_B meisaiPrintB = new B0420_SeikyuMeisaishoPrint_B();
+            try
+            {
+                // 検索するデータをリストに格納
+                lstSearchItem.Add(txtSimekiriYmd.Text);
+                lstSearchItem.Add(txtStartYmd.Text);
+                lstSearchItem.Add(txtSimekiribi.Text);
+                lstSearchItem.Add(labelSet_TokuisakiCdFrom.CodeTxtText);
+                lstSearchItem.Add(labelSet_TokuisakiCdTo.CodeTxtText);
+                lstSearchItem.Add(Environment.UserName);
+
+                // 検索実行
+                DataTable dtSeikyuMeisai = meisaiPrintB.addSeikyuMeisaisho(lstSearchItem);
+
+                if (dtSeikyuMeisai != null && dtSeikyuMeisai.Rows.Count > 0)
+                {
+                    // PDF作成
+                    String strFile = meisaiPrintB.dbToPdf(dtSeikyuMeisai);
+
+                    // 印刷ダイアログ
+                    Common.Form.PrintForm pf = new Common.Form.PrintForm(this, strFile, SIZE_A4, false);
+                    pf.ShowDialog();
+                    pf.Dispose();
+                }
+                else
+                {
+                    // メッセージボックスの処理、対象データがない場合のウィンドウ（OK）
+                    BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_VIEW, "対象のデータはありません。", CommonTeisu.BTN_OK, CommonTeisu.DIAG_INFOMATION);
+                    basemessagebox.ShowDialog();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // エラーロギング
+                new CommonException(ex);
+
+                // メッセージボックスの処理、PDF作成失敗の場合のウィンドウ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_VIEW, "印刷が失敗しました。", CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                return;
+            }
+        }
+
+        /// <summary>
+        /// blnDataCheck
+        /// データチェック
+        /// </summary>
+        private Boolean blnDataCheck()
+        {
+           // 空文字判定（締切年月日）
+            if (txtSimekiriYmd.blIsEmpty() == false)
+            {
+                // メッセージボックスの処理、項目が空の場合のウィンドウ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_INPUT, "項目が空です。日付を入力してください。", CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                txtSimekiriYmd.Focus();
+
+                return false;
+            }
+
+            // 空文字判定（開始年月日）
+            if (txtStartYmd.blIsEmpty() == false)
+            {
+                // メッセージボックスの処理、項目が空の場合のウィンドウ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_INPUT, "項目が空です。日付を入力してください。", CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                txtStartYmd.Focus();
+
+                return false;
+            }
+
+            // 空文字判定（締切日コード）
+            if (txtSimekiribi.Text.Equals(""))
+            {
+                // メッセージボックスの処理、項目が空の場合のウィンドウ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_INPUT, CommonTeisu.LABEL_NULL, CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                txtSimekiribi.Focus();
+
+                return false;
+            }
+
+            // 空文字判定（得意先コード（開始））
+            if (labelSet_TokuisakiCdFrom.CodeTxtText.Equals(""))
+            {
+                // メッセージボックスの処理、項目が空の場合のウィンドウ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_INPUT, CommonTeisu.LABEL_NULL, CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                labelSet_TokuisakiCdFrom.Focus();
+
+                return false;
+            }
+
+            // 空文字判定（得意先コード（終了））
+            if (labelSet_TokuisakiCdTo.CodeTxtText.Equals(""))
+            {
+                // メッセージボックスの処理、項目が空の場合のウィンドウ（OK）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_INPUT, CommonTeisu.LABEL_NULL, CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                basemessagebox.ShowDialog();
+
+                labelSet_TokuisakiCdTo.Focus();
+
+                return false;
+            }
+
+            // 締切年月日の日付と締切日コードが違う場合
+            if (!DateTime.Parse(txtSimekiriYmd.Text).Day.Equals(int.Parse(txtSimekiribi.Text)))
+            {
+                // メッセージボックスの処理（YES,NO）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, "締日付の確認", "締切年月日の日付と締切日コードが違います。" + "\r\n" + "続行しますか？", CommonTeisu.BTN_YESNO, CommonTeisu.DIAG_QUESTION);
+
+                // Noが押された場合
+                if (basemessagebox.ShowDialog() == DialogResult.No)
+                {
+                    return false;
+                }
+            }
+
+            // 再発行の場合
+            if (radSet_2btnSyurui.radbtn1.Checked)
+            {
+                // メッセージボックスの処理（YES,NO）
+                BaseMessageBox basemessagebox = new BaseMessageBox(this, "再発行の確認", "再発行をすると前回の請求履歴が破棄されます。" + "\r\n" + "よろしいですか？。", CommonTeisu.BTN_YESNO, CommonTeisu.DIAG_QUESTION);
+
+                // Noが押された場合
+                if (basemessagebox.ShowDialog() == DialogResult.No)
+                {
+                    return false;
+                }
+            }
+
+            // 通常発行の場合
+            if (radSet_2btnSyurui.radbtn0.Checked)
+            {
+                // データ検索用
+                List<string> lstSearchItem = new List<string>();
+
+                // 検索するデータをリストに格納
+                lstSearchItem.Add(labelSet_TokuisakiCdFrom.CodeTxtText);
+                lstSearchItem.Add(labelSet_TokuisakiCdTo.CodeTxtText);
+                lstSearchItem.Add(txtSimekiriYmd.Text);
+
+                // ビジネス層のインスタンス生成
+                B0420_SeikyuMeisaishoPrint_B meisaiPrintB = new B0420_SeikyuMeisaishoPrint_B();
+                try
+                {
+                    // 請求履歴を取得
+                    DataTable dtSeikyuRireki = meisaiPrintB.getSeikyuRireki(lstSearchItem);
+
+                    if (dtSeikyuRireki != null && dtSeikyuRireki.Rows.Count > 0)
+                    {
+                        // 請求履歴に登録されている場合
+                        if (!dtSeikyuRireki.Rows[0][0].Equals(0))
+                        {
+                            // メッセージボックスの処理、請求履歴に登録されている場合のウィンドウ（OK）
+                            BaseMessageBox basemessagebox = new BaseMessageBox(this, "通常発行の確認", "請求明細書はすでに発行済みです。" + "\r\n" + "取引先の範囲を限定するか、再発行をしてください。", CommonTeisu.BTN_OK, CommonTeisu.DIAG_INFOMATION);
+                            basemessagebox.ShowDialog();
+
+                            return false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // エラーロギング
+                    new CommonException(ex);
+
+                    // 例外発生メッセージ（OK）
+                    BaseMessageBox basemessagebox = new BaseMessageBox(this, CommonTeisu.TEXT_ERROR, CommonTeisu.LABEL_ERROR_MESSAGE, CommonTeisu.BTN_OK, CommonTeisu.DIAG_ERROR);
+                    basemessagebox.ShowDialog();
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
